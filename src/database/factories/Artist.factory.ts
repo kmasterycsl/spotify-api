4bd9292b98206a7d0a4dcaad02c99d746eaf3616
Khanh Tran <tranduykhanhyc@gmail.com>
2021-08-21 23:26:32 +0700

    feat: add artist/asset models

---
+++ src/database/factories/Artist.factory.ts
@@ -0,0 +1,17 @@
+import Faker from 'faker'
+import { Artist } from 'src/modules/artist/entities/artist.entity'
+import { Asset, AssetType } from 'src/modules/asset/entities/asset.entity'
+import { define, factory } from 'typeorm-seeding'
+
+define(Artist, (faker: typeof Faker) => {
+    const name = faker.name.firstName() + ' ' + faker.name.lastName();
+    const biography = faker.lorem.sentence();
+
+    const artist = new Artist();
+    artist.name = name;
+    artist.biography = biography;
+    artist.avatarImage = factory(Asset)({ kind: 'artist-avatar', type: AssetType.IMAGE }) as any;
+    artist.coverImage = factory(Asset)({ kind: 'artist-cover', type: AssetType.IMAGE }) as any;
+
+    return artist;
+})
---
+++ src/database/factories/Asset.factory.ts
@@ -0,0 +1,29 @@
+import Faker from 'faker'
+import { Asset, AssetType, IImageMeta } from 'src/modules/asset/entities/asset.entity'
+import { define } from 'typeorm-seeding'
+
+define(Asset, (faker: typeof Faker, context: { type: AssetType, kind: 'artist-avatar' | 'artist-cover' }) => {
+    const asset = new Asset<IImageMeta>();
+    asset.type = context.type;
+
+    switch (context.kind) {
+        case 'artist-avatar':
+            asset.meta = {
+                height: 128,
+                width: 128,
+                source: `https://picsum.photos/${128}/${128}?random=${Math.random()}&grayscale&blur=2`,
+            }
+            break;
+        case 'artist-cover':
+            asset.meta = {
+                height: 360,
+                width: 850,
+                source: `https://picsum.photos/${360}/${850}?random=${Math.random()}&grayscale&blur=2`,
+            }
+            break;
+        default:
+            throw new Error('Kind is not supported');
+    }
+
+    return asset;
+})
