a0a0c70aedf472cacd63b88510c9fc8b5adabf0c
Khanh Tran <tranduykhanhyc@gmail.com>
2021-08-22 14:10:13 +0700

    paginate artists

--- src/common/PaginationArgs.ts
+++ src/common/PaginationArgs.ts
@@ -3,7 +3,7 @@ import { ArgsType, Int, Field } from "@nestjs/graphql";
 @ArgsType()
 export class PaginationArgs {
   @Field((type) => Int)
-  offset: number = 0;
+  page: number = 0;
 
   @Field((type) => Int)
   limit: number = 10;
--- src/modules/artist/models/artist.model.ts
+++ src/modules/artist/models/artist.model.ts
@@ -1,6 +1,7 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { AssetEntity, IImageMeta } from 'src/modules/asset/entities/asset.entity';
 import { Asset } from 'src/modules/asset/models/asset.model';
+import { Paginated } from '../args/Paginated';
 
 @ObjectType()
 export class Artist {
@@ -22,3 +23,6 @@ export class Artist {
   @Field(type => Asset)
   avatarImage: AssetEntity<IImageMeta>;
 }
+
+@ObjectType()
+export class PaginatedArtist extends Paginated(Artist) {}
