e1c35380b14ffc8b1d977c5fa105f3188d274560
Khanh Tran <tranduykhanhyc@gmail.com>
2021-08-22 16:32:15 +0700

    paginate tracks

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArtistToTrack } from './artist-to-track.entity';
+import { Track } from './track.entity';
+import { TrackService } from './track.service';
 
-@Module({})
-export class TrackModule {}
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([Track, ArtistToTrack]),
+    ],
+    providers: [TrackService],
+    exports: [TrackService]
+})
+export class TrackModule { }
