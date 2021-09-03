bb5e8e9dbc8efaf7f72f16c0a71159d1e320899e
Khanh Tran <khanh.tran@canalcircle.com>
2021-09-03 23:01:58 +0700

    fix

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -5,11 +5,5 @@ import { UserService } from './user.service';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) { }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('me')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 }
 
