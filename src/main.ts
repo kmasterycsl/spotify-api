32ccf0d49374b6442628b5713b8798c2dd555368
Khanh Tran <khanh.tran@canalcircle.com>
2021-09-13 00:09:21 +0700

    add full likeable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { OptionalGqlAuthGuard } from "./modules/user/strategies/optional-graphql.guard";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalGuards(new OptionalGqlAuthGuard());
     await app.listen(3000);
 }
 bootstrap();
