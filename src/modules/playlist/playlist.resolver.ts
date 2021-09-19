62b0672b2345541b586ac63fd7a36ab75999b0e8
Khanh Tran <khanh.tran@canalcircle.com>
2021-09-19 13:27:31 +0700

    playlist

--- src/modules/playlist/playlist.resolver.ts
+++ src/modules/playlist/playlist.resolver.ts
@@ -1,9 +1,10 @@
 import { UseGuards } from "@nestjs/common";
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
 import { TrackService } from "src/modules/track/track.service";
 import { CurrentUser } from "src/shared/decorators/current-user.decorator";
 import { GqlAuthGuard } from "../user/strategies/graphql.guard";
 import { User } from "../user/user.entity";
+import { AddTrackToPlaylist } from "./args/AddTrackToPlaylist.arg";
 import { CreatePlaylistArgs } from "./args/CreatePlaylist.arg";
 import { Playlist } from "./playlist.entity";
 import { PlaylistService } from "./playlist.service";
@@ -15,6 +16,12 @@ export class PlaylistResolver {
         private readonly trackService: TrackService
     ) {}
 
+    @Query(() => [Playlist])
+    @UseGuards(GqlAuthGuard)
+    async getOwnPlaylists(@CurrentUser() user: User) {
+        return this.playlistService.findByUserId(user.id);
+    }
+
     @Mutation(() => Playlist)
     @UseGuards(GqlAuthGuard)
     async createPlaylist(
@@ -22,5 +29,19 @@ export class PlaylistResolver {
         @CurrentUser() user: User
     ): Promise<Playlist> {
         return this.playlistService.createPlaylist(args, user);
+    }
+
+    @Mutation(() => Playlist)
+    @UseGuards(GqlAuthGuard)
+    async addTrackToPlaylist(
+        @Args() args: AddTrackToPlaylist,
+        @CurrentUser() user: User
+    ): Promise<Playlist> {
+        return this.playlistService.addTrackToPlaylist(args, user);
+    }
+
+    @ResolveField(() => Number)
+    async tracksCount(@Parent() playlist: Playlist): Promise<number> {
+        return this.playlistService.getTracksCount(playlist.id);
     }
 }
--- src/modules/playlist/playlist.service.ts
+++ src/modules/playlist/playlist.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { ForbiddenError } from "apollo-server-errors";
 import { getManager, Repository } from "typeorm";
 import { Likeable, LikeableType } from "../likeable/likeable.entity";
+import { Track } from "../track/track.entity";
 import { User } from "../user/user.entity";
+import { AddTrackToPlaylist } from "./args/AddTrackToPlaylist.arg";
 import { CreatePlaylistArgs } from "./args/CreatePlaylist.arg";
 import { PlaylistToTrack } from "./playlist-to-track.entity";
 import { Playlist } from "./playlist.entity";
@@ -12,12 +15,30 @@ export class PlaylistService {
     constructor(
         @InjectRepository(Playlist)
         private playlistsRepository: Repository<Playlist>,
+        @InjectRepository(Track)
+        private tracksRepository: Repository<Track>,
         @InjectRepository(PlaylistToTrack)
         private playlistToTrackRepository: Repository<PlaylistToTrack>
     ) {}
 
+    async findByUserId(userId: string): Promise<Playlist[]> {
+        const playlists = await this.playlistsRepository.find({
+            where: {
+                userId,
+            },
+            order: {
+                createdAt: "DESC",
+            },
+        });
+        return playlists;
+    }
+
     findOneById(id: string): Promise<Playlist> {
         return this.playlistsRepository.findOne(id);
+    }
+
+    getTracksCount(playlistId: string): Promise<number> {
+        return this.playlistToTrackRepository.count({ playlistId });
     }
 
     async createPlaylist(arg: CreatePlaylistArgs, user: User) {
@@ -36,6 +57,35 @@ export class PlaylistService {
             await txnEntityManager.save(likeable);
 
             return playlist;
+        });
+
+        return playlist;
+    }
+
+    async addTrackToPlaylist(arg: AddTrackToPlaylist, user: User) {
+        const [playlist, track, latestPlaylistToTrack] = await Promise.all([
+            this.playlistsRepository.findOneOrFail(arg.playlistId),
+            this.tracksRepository.findOneOrFail(arg.trackId),
+            this.playlistToTrackRepository.findOne(
+                {
+                    playlistId: arg.playlistId,
+                },
+                {
+                    order: {
+                        order: "DESC",
+                    },
+                }
+            ),
+        ]);
+
+        if (playlist.userId !== user.id) {
+            throw new ForbiddenError("Can't add track to other's playlist");
+        }
+
+        await this.playlistToTrackRepository.save({
+            playlistId: playlist.id,
+            trackId: track.id,
+            order: latestPlaylistToTrack ? latestPlaylistToTrack.order + 1 : 0,
         });
 
         return playlist;
