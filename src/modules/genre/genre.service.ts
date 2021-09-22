ba0fc041cfe86233a8e5589dcfbbdd90f6a96e4a
Khanh Tran <khanh.tran@canalcircle.com>
2021-09-23 01:20:59 +0700

    genre should not paginated

--- src/modules/genre/genre.service.ts
+++ src/modules/genre/genre.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { paginate } from "nestjs-typeorm-paginate";
-import { Pagination } from "src/shared/Pagination";
-import convertToCustomPagination from "src/shared/utils/convertToCustomPagination";
 import { Repository } from "typeorm";
-import { GetGenresArgs } from "./args/GetGenres.arg";
 import { GenreToPlaylist } from "./genre-to-playlist.entity";
 import { Genre } from "./genre.entity";
 @Injectable()
@@ -16,11 +12,8 @@ export class GenreService {
         private genreToPlaylistRepository: Repository<GenreToPlaylist>
     ) {}
 
-    find(args: GetGenresArgs): Promise<Pagination<Genre>> {
-        return paginate<Genre>(this.genresRepository, {
-            limit: args.limit,
-            page: args.page,
-        }).then(convertToCustomPagination);
+    find(): Promise<Genre[]> {
+        return this.genresRepository.find();
     }
 
     findOneById(id: string): Promise<Genre> {
