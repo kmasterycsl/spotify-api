c84b4dbb455cf183d9fccb64fcfb6cf0a7756f8a
Khanh Tran <khanh.tran@canalcircle.com>
2021-10-04 07:28:57 +0700

    fixed

--- src/modules/album/args/GetAlbums.args.ts
+++ src/modules/album/args/GetAlbums.args.ts
@@ -3,6 +3,6 @@ import { PaginationArgs } from "src/shared/args/PaginationArgs";
 
 @ArgsType()
 export class GetAlbumsArgs extends PaginationArgs {
-    @Field(type => String)
+    @Field(type => String, { nullable: true })
     query?: string;
 }
--- src/modules/playlist/args/GetPlaylistsArgs.arg.ts
+++ src/modules/playlist/args/GetPlaylistsArgs.arg.ts
@@ -3,6 +3,6 @@ import { PaginationArgs } from "src/shared/args/PaginationArgs";
 
 @ArgsType()
 export class GetPlaylistsArgs extends PaginationArgs {
-    @Field(type => String)
+    @Field(type => String, { nullable: true })
     query?: string;
 }
